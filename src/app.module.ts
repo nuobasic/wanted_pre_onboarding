8d560b17bd80b91bbb2452bfbc856c18a10aca1d
nuobasic <nuobasic@naver.com>
2022-09-30 17:47:57 +0900

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASES,
+      entities: [],
+      synchronize: true,
+      logging: true,
+    })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
